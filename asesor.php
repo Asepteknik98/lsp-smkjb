2b020c2e4e443a7a6a9504c8303bac7967bce49c
bangsam566 <samanixzy@gmail.com>
2025-10-29 22:49:17 +0700

    update asesor.php

--- asesor.php
+++ asesor.php
@@ -27,26 +27,27 @@
                     <tr>
                         <th class="px-4 py-2 text-left text-sm font-medium">No</th>
                         <th class="px-4 py-2 text-left text-sm font-medium">Nama Asesor</th>
-                        <th class="px-4 py-2 text-left text-sm font-medium">Nomor Registrasi</th>
+                        <th class="px-4 py-2 text-left text-sm font-medium">NIP</th>
                         <th class="px-4 py-2 text-left text-sm font-medium">Skema Kompetensi</th>
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">1</td><td class="px-4 py-2">Iqbal Qurnawan, S.Pd.</td><td class="px-4 py-2">ASKB-2025-001</td><td class="px-4 py-2">Operator Perkakas Mesin</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">2</td><td class="px-4 py-2">Rohmad Kuswindarto, S.Pd.</td><td class="px-4 py-2">ASKB-2025-002</td><td class="px-4 py-2">Operator Perkakas Mesin</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">3</td><td class="px-4 py-2">Aan Farhan, S.Ag</td><td class="px-4 py-2">ASKB-2025-003</td><td class="px-4 py-2">Operator Pemasangan Instalasi Listrik</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">4</td><td class="px-4 py-2">Mahdi, S.Si</td><td class="px-4 py-2">ASKB-2025-004</td><td class="px-4 py-2">Operator Pemasangan Instalasi Listrik</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">5</td><td class="px-4 py-2">Lukmanul Hakim, S.T.</td><td class="px-4 py-2">ASKB-2025-005</td><td class="px-4 py-2">Teknik Sepeda Motor</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">6</td><td class="px-4 py-2">Keke Asbita Vitaloka, S.Pd</td><td class="px-4 py-2">ASKB-2025-006</td><td class="px-4 py-2">Teknik Sepeda Motor</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">7</td><td class="px-4 py-2">Egi Andiana, M.M.</td><td class="px-4 py-2">ASKB-2025-007</td><td class="px-4 py-2 italic">Junior Custom Made</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">8</td><td class="px-4 py-2">Abdul Mutholib, S.M.</td><td class="px-4 py-2">ASKB-2025-008</td><td class="px-4 py-2 italic">Junior Custom Made</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">9</td><td class="px-4 py-2">Agus Solihin, S.E</td><td class="px-4 py-2">ASKB-2025-009</td><td class="px-4 py-2 break-words">Tukang Las Konstruksi Plat dan Pipa Level 2</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">10</td><td class="px-4 py-2">Widya Ningsih, S.Pd.</td><td class="px-4 py-2">ASKB-2025-010</td><td class="px-4 py-2 break-words">Tukang Las Konstruksi Plat dan Pipa Level 2</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">11</td><td class="px-4 py-2">Rofiq Okvianto, M.Pd</td><td class="px-4 py-2">ASKB-2025-011</td><td class="px-4 py-2 italic">Junior Drafter Fiber Optik</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">12</td><td class="px-4 py-2">Imas Rahmawati, S.Pd.</td><td class="px-4 py-2">ASKB-2025-012</td><td class="px-4 py-2 italic">Junior Drafter Fiber Optik</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">13</td><td class="px-4 py-2">Suhendi, S.Pd.</td><td class="px-4 py-2">ASKB-2025-013</td><td class="px-4 py-2">Teknisi Refrigasi dan Tata Udara</td></tr>
-                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">14</td><td class="px-4 py-2">Arif Effendi, S.Pd.</td><td class="px-4 py-2">ASKB-2025-014</td><td class="px-4 py-2">Teknisi Refrigasi dan Tata Udara</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">1</td><td class="px-4 py-2">Iqbal Qurnawan</td><td class="px-4 py-2">198505150001000001</td><td class="px-4 py-2">Operator Perkakas Mesin</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">2</td><td class="px-4 py-2">Rohmad Kuswindarto</td><td class="px-4 py-2">199003220002000010</td><td class="px-4 py-2">Operator Perkakas Mesin</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">3</td><td class="px-4 py-2">Aan Farhan</td><td class="px-4 py-2">197812010003000029</td><td class="px-4 py-2">Operator Pemasangan Instalasi Listrik</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">4</td><td class="px-4 py-2">Mahdi</td><td class="px-4 py-2">199706050004000038</td><td class="px-4 py-2">Operator Pemasangan Instalasi Listrik</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">5</td><td class="px-4 py-2">Lukmanul Hakim</td><td class="px-4 py-2">198912300005000047</td><td class="px-4 py-2">Teknik Sepeda Motor</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">6</td><td class="px-4 py-2">Keke Asbita Vitaloka</td><td class="px-4 py-2">199402140006000056</td><td class="px-4 py-2">Teknik Sepeda Motor</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">7</td><td class="px-4 py-2">Egi Andiana</td><td class="px-4 py-2">198207090007000065</td><td class="px-4 py-2 italic">Junior Custom Made</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">8</td><td class="px-4 py-2">Abdul Mutholib</td><td class="px-4 py-2">199811110008000074</td><td class="px-4 py-2 italic">Junior Custom Made</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">9</td><td class="px-4 py-2">Agus Solihin</td><td class="px-4 py-2">197505250009000083</td><td class="px-4 py-2 break-words">Tukang Las Konstruksi Plat dan Pipa Level 2</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">10</td><td class="px-4 py-2">Widya Ningsih</td><td class="px-4 py-2">199609300010000092</td><td class="px-4 py-2 break-words">Tukang Las Konstruksi Plat dan Pipa Level 2</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">11</td><td class="px-4 py-2">Rofiq Okvianto</td><td class="px-4 py-2">198601200011000101</td><td class="px-4 py-2 italic">Junior Drafter Fiber Optik</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">12</td><td class="px-4 py-2">Imas Rahmawati</td><td class="px-4 py-2">199212180012000110</td><td class="px-4 py-2 italic">Junior Drafter Fiber Optik</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">13</td><td class="px-4 py-2">Suhendi</td><td class="px-4 py-2">198310070013000129</td><td class="px-4 py-2">Teknisi Refrigasi dan Tata Udara</td></tr>
+                    <tr class="hover:bg-gray-100"><td class="px-4 py-2">14</td><td class="px-4 py-2">Arif Effendi</td><td class="px-4 py-2">198703152014000140</td><td class="px-4 py-2">Teknisi Refrigasi dan Tata Udara</td></tr>
                 </tbody>
+
             </table>
         </div>
     </main>
